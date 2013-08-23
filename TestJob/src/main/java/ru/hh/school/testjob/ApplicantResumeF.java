2232969cd1ffaa098577a8c483444859ab84186f
Max S <maxmotos@mts.ru>
2013-08-23 15:42:09 +0400

    ready layouts

---
+++ TestJob/src/main/java/ru/hh/school/testjob/ApplicantResumeF.java
@@ -0,0 +1,11 @@
+package ru.hh.school.testjob;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mvsereb3
+ * Date: 23.08.13
+ * Time: 15:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class ApplicantResumeF {
+}
---
+++ TestJob/src/main/java/ru/hh/school/testjob/EmployerResumeF.java
@@ -0,0 +1,11 @@
+package ru.hh.school.testjob;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mvsereb3
+ * Date: 23.08.13
+ * Time: 15:01
+ * To change this template use File | Settings | File Templates.
+ */
+public class EmployerResumeF {
+}
